e540c4e1a93d448ed810d9741862a183ed2e881f
Morshaedul <morshaedulanirbanit@gmail.com>
2022-05-11 20:04:47 +0600

    first commit

---
+++ js/eqMethod.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+    $(".para").eq(-1).css({ "color": "red", "fontSize": "20px", "fontWeight": "bold" });
+    $(".para").eq(-3).css({ "color": "blue", "fontSize": "20px", "fontWeight": "bold" });
+    });
